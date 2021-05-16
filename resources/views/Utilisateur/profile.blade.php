216faba03672c33dbd96f3bc6fc359a2278f5517
ASSANI Vanessa <assanizekia0203@gmail.com>
2021-05-17 00:42:29 +0200

    niveau

--- resources/views/Utilisateur/profile.blade.php
+++ resources/views/Utilisateur/profile.blade.php
@@ -1,6 +1,21 @@
 @extends('Utilisateur/gabarit_profile')
 @section('contenu')
-
+<hr class="separation"/>
+            <div class="table-wrapper mt-5">
+                <table class="table_licence_gene" cellspacing="1" cellpadding="1" id="table_master" >
+                    <tr id="niv">
+                       <th rowspan="2"> Scolarité </th>
+                    </tr>
+                    <tr class="sem">
+                        <td> Semestre 1 </td>
+                        <td> Semestre 2 </td>
+                        <td> Semestre 3 </td>
+                        <td> Semestre 4 </td>
+                        <td> Semestre 5 </td>
+                        <td> Semestre 6 </td>
+                    </tr>
+                </table>
+            </div>
 <script type="text/javascript">
     $('#categorie').change(function(){
         var categorieID = $(this).val();
@@ -11,10 +26,13 @@
                 success: function(res){
                     if(res){
                         $("#niveau").empty();
+                        $("#niv").empty();
                         $("#niveau").append('<option>Niveau </option>');
+                        $("#niv").append('<th rowspan="2" > Scolarité </th>');
                         $.each(res, function(key,value){
                             $("#niveau").append('<option value="'+key+'">'+value+'</option>');
-                            $("#niv").append('<th>'+value+'</th>');
+                            $("#niv").append('<th colspan="2">'+value+'</th>');
+                            $(".sem").append('')
                         });
                     }else{
                         $("#niveau").empty();
