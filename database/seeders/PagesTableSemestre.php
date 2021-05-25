950e5c1eb7373acac4a75a831b5d2f6f56d97c38
MAILLOCHON Corentin <corentin.maillochon12@gmail.com>
2021-05-25 19:09:23 +0200

    modif semestre

--- database/seeders/PagesTableSemestre.php
+++ database/seeders/PagesTableSemestre.php
@@ -18,52 +18,52 @@ class PagesTableSemestre extends Seeder
         DB::table('semestres')->insert([
             [
                 'nom_semestre' => 'Semestre 1',
-                'id_niveau' => null
+                'id_niveau' => 1
             ],[
                 'nom_semestre' => 'Semestre 2',
-                'id_niveau' => null
+                'id_niveau' => 1
             ],[
                 'nom_semestre' => 'Semestre 3',
-                'id_niveau' => null
+                'id_niveau' => 2
             ],[
                 'nom_semestre' => 'Semestre 4',
-                'id_niveau' => null
+                'id_niveau' => 2
             ],[
                 'nom_semestre' => 'Semestre 5',
-                'id_niveau' => null
+                'id_niveau' => 3
             ],[
                 'nom_semestre' => 'Semestre 6',
-                'id_niveau' => null
+                'id_niveau' => 3
             ],[
                 'nom_semestre' => 'UE 0',
-                'id_niveau' => null
+                'id_niveau' => 4
             ],[
                 'nom_semestre' => 'UE 1',
-                'id_niveau' => null
+                'id_niveau' => 4
             ],[
                 'nom_semestre' => 'UE 2',
-                'id_niveau' => null
+                'id_niveau' => 4
             ],[
                 'nom_semestre' => 'UE 3',
-                'id_niveau' => null
+                'id_niveau' => 4
             ],[
                 'nom_semestre' => 'UE 4',
-                'id_niveau' => null
+                'id_niveau' => 4
             ],[
                 'nom_semestre' => 'UE 5',
-                'id_niveau' => null
+                'id_niveau' => 4
             ],[
                 'nom_semestre' => 'UE 6',
-                'id_niveau' => null
+                'id_niveau' => 4
             ],[
                 'nom_semestre' => 'UE 7',
-                'id_niveau' => null
+                'id_niveau' => 4
             ],[
                 'nom_semestre' => 'UE 8',
-                'id_niveau' => null
+                'id_niveau' => 4
             ],[
                 'nom_semestre' => 'UE 9',
-                'id_niveau' => null
+                'id_niveau' => 4
             ]
         ]);
     }
