0ad51e316f97ee6f7ee74d4e3d8e93ec606c2d6d
MAILLOCHON Corentin <corentin.maillochon12@gmail.com>
2021-05-25 19:07:41 +0200

    modif prof

--- database/seeders/PagesTableNiveauEtude.php
+++ database/seeders/PagesTableNiveauEtude.php
@@ -18,27 +18,27 @@ class PagesTableNiveauEtude extends Seeder
             [
                 'nom_niveau' => 'Licence 1',
                 'id_categorie' => null,
-                'responsable_annee' => null
+                'responsable_annee' => 1
             ],[
                 'nom_niveau' => 'Licence 2',
                 'id_categorie' => null,
-                'responsable_annee' => null
+                'responsable_annee' => 18
             ],[
                 'nom_niveau' => 'Licence 3',
                 'id_categorie' => null,
-                'responsable_annee' => null,
+                'responsable_annee' => 12
             ],[
                 'nom_niveau' => 'Licence 3 Pro',
                 'id_categorie' => null,
-                'responsable_annee' => null
+                'responsable_annee' => 9
             ],[
                 'nom_niveau' => 'Master 1',
                 'id_categorie' => null,
-                'responsable_annee' => null,
+                'responsable_annee' => 7,
             ],[
                 'nom_niveau' => 'Master 2',
                 'id_categorie' => null,
-                'responsable_annee' => null
+                'responsable_annee' => 11
             ]
         ]);
     }
